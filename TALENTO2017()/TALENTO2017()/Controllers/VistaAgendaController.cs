ab0bf5be70818989e4490bad3cb41d245f35b9ba
gonaso <alexg_13_92@hotmail.com>
2017-11-01 01:16:42 -0300

    2º COMMIT

---
+++ TALENTO2017()/TALENTO2017()/Controllers/VistaAgendaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TALENTO2017__.Controllers
+{
+    public class VistaAgendaController : Controller
+    {
+        // GET: VistaAgenda
+        public ActionResult Agenda()
+        {
+            return View();
+        }
+    }
+}
---
+++ TALENTO2017()/TALENTO2017()/Controllers/VistaPerfilController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TALENTO2017__.Controllers
+{
+    public class VistaPerfilController : Controller
+    {
+        // GET: VistaPerfil
+        public ActionResult VistaPerfil()
+        {
+            return View();
+        }
+    }
+}
